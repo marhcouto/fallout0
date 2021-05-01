a93ad52eceefbb9df0e29afd7215fb7f55710c5a
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-01 18:58:41 +0100

    Updated main class

--- src/main/java/com/lpoo/fallout/Game.java
+++ src/main/java/com/lpoo/fallout/Game.java
@@ -1,7 +1,9 @@
 package com.lpoo.fallout;
 
+import java.io.IOException;
+
 public class Game {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Fallout!");
     }
 }
