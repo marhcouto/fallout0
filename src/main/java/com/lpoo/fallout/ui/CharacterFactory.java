e75f39b7247b53f11fe33af00961d1ab6ce9bd94
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-01 18:43:06 +0100

    Created class to handle asset creation

---
+++ src/main/java/com/lpoo/fallout/ui/CharacterFactory.java
@@ -0,0 +1,16 @@
+package com.lpoo.fallout.ui;
+
+import com.lpoo.fallout.data.Element;
+
+
+public class CharacterFactory {
+    private CharacterFactory(){};
+
+    public static LCharacter createCharacter(Element element) {
+        LCharacter character = null;
+        //TODO Depois de estarem definidos os personagens criar método
+        return character;
+    }
+
+
+}
