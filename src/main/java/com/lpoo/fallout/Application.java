d773606021f1b4b9765a6b8ef5eafb68d6ffa471
marhcouto <marhcouto1101@gmail.com>
2021-05-03 23:13:09 +0100

    Updated

--- src/main/java/com/lpoo/fallout/Application.java
+++ src/main/java/com/lpoo/fallout/Application.java
@@ -5,12 +5,14 @@ import com.lpoo.fallout.view.GameView;
 
 import java.io.IOException;
 
-public class Game {
+public class Application {
     private LanternaTerminal terminal; //TODO: provavelmente é um singleton porque se criarmos outro terminal a janela vai fechar e abrir a meio do jogo e fica feio
     private GameView view;
     //TODO: Aqui há de estar um controller tmb
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello Fallout!");
+
     }
 }
+
---
+++ src/main/java/com/lpoo/fallout/controller/Game.java
@@ -0,0 +1,8 @@
+package com.lpoo.fallout.controller;
+
+public class Game {
+
+    public void Game() {
+
+    }
+}
