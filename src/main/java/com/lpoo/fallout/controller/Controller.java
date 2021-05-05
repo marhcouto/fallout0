7399188f5147d10c9c28716f6ddd6432723a45f3
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-05 22:22:26 +0100

    Provided necessary MVC interfaces

---
+++ src/main/java/com/lpoo/fallout/controller/Controller.java
@@ -0,0 +1,5 @@
+package com.lpoo.fallout.controller;
+
+public interface Controller {
+    void run();
+}
---
+++ src/main/java/com/lpoo/fallout/view/Viewer.java
@@ -0,0 +1,7 @@
+package com.lpoo.fallout.view;
+
+import java.io.IOException;
+
+public interface Viewer {
+    void draw() throws IOException;
+}
