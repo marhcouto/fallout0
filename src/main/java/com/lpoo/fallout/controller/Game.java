985ca20555df9ef20e0de700639dc05acafcd996
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-05 22:20:56 +0100

    Added classes to handle state change at the main state machine

--- src/main/java/com/lpoo/fallout/controller/Game.java
+++ src/main/java/com/lpoo/fallout/controller/Game.java
@@ -1,8 +1,40 @@
 package com.lpoo.fallout.controller;
 
+import java.util.Stack;
+
 public class Game {
+    private Stack<Controller> controllers;
+    private static final Integer FPS = 60;
+    private final long frameTime;
 
-    public void Game() {
+    public Game() {
+        controllers = new Stack<>();
+        frameTime = 1000/60;
+    }
 
+    public void pushController(Controller newController) {
+        controllers.push(newController);
+    }
+    public void popController() {
+        controllers.pop();
+    }
+
+    public void clearControllers() {
+        controllers.clear();
+    }
+
+    public void run() {
+        while (!controllers.empty()) {
+            long startTime = System.currentTimeMillis();
+            controllers.peek().run();
+
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long sleepTime = frameTime - elapsedTime;
+            if (sleepTime > 0) try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
