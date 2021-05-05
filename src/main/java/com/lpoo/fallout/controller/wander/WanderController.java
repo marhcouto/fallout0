581ab9a73f591f5e491ea6d1b0bc55219adf7e40
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-05 22:23:43 +0100

    Created the class that will represent Wanderer status controller

---
+++ src/main/java/com/lpoo/fallout/controller/wander/WanderController.java
@@ -0,0 +1,11 @@
+package com.lpoo.fallout.controller.wander;
+
+import com.lpoo.fallout.controller.Controller;
+
+public class WanderController implements Controller {
+
+    @Override
+    public void run() {
+
+    }
+}
