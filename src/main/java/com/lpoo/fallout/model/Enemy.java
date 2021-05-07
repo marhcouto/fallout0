b292bd7f3fbb52867ded02196c999ca882e14f58
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-07 18:23:06 +0100

    Added types to enemies. Used to know how to draw them

--- src/main/java/com/lpoo/fallout/model/Enemy.java
+++ src/main/java/com/lpoo/fallout/model/Enemy.java
@@ -4,19 +4,25 @@ import com.lpoo.fallout.controller.wander.MovingEngine;
 
 public abstract class Enemy extends Character {
     private MovingEngine movingEngine;
+    private TYPE enemyType;
 
-    public Enemy(Position position, Attributes attributes, Integer level) {
+    public Enemy(Position position, Attributes attributes, Integer level, TYPE enemyType) {
         super(position, attributes, level);
+        this.enemyType = enemyType;
     }
 
-    public Enemy(Position position, Attributes attributes, Weapon weapon, Integer level) {
+    public Enemy(Position position, Attributes attributes, Weapon weapon, Integer level, TYPE enemyType) {
         super(position, attributes, weapon, level);
+        this.enemyType = enemyType;
+    }
+
+    TYPE getType() {
+        return enemyType;
     }
 
     public MovingEngine getMovingEngine() {
         return movingEngine;
     }
 
-    @Override
-    protected abstract LanternaDrawable imageCreator();
+    public enum TYPE { SCORPION, RAT }
 }
