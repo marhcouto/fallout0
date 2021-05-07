ce827f130cbff99bdaf2f74cc13814a39dcdbe35
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-07 18:24:22 +0100

    Updated LanternaDrawable to be null safe

--- src/main/java/com/lpoo/fallout/model/LanternaDrawable.java
+++ src/main/java/com/lpoo/fallout/model/LanternaDrawable.java
@@ -1,6 +1,6 @@
 package com.lpoo.fallout.model;
 
-import com.lpoo.fallout.model.Position;
+import org.jetbrains.annotations.NotNull;
 
 public class LanternaDrawable {
     Position position;
@@ -8,10 +8,15 @@ public class LanternaDrawable {
     private final String backgroundColor;
     private final String img;
 
-    public LanternaDrawable(Position position, String foregroundColor, String backgroundColor, String img) {
+    public LanternaDrawable(@NotNull String foregroundColor, @NotNull String backgroundColor, @NotNull String img) {
+        this(new Position(0, 0), foregroundColor, backgroundColor, img);
+    }
+
+    public LanternaDrawable(@NotNull Position position, @NotNull String foregroundColor, @NotNull String backgroundColor, @NotNull String img) {
         this.foregroundColor = foregroundColor;
         this.backgroundColor = backgroundColor;
         this.img = img;
+        this.position = position;
     }
 
     public String getForegroundColor() {
