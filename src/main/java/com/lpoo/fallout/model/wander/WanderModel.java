9185117b2bbca3e4fa503947a77e00611eacf22d
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-07 11:39:49 +0100

    Added not null checks Added a basic colision detector

--- src/main/java/com/lpoo/fallout/model/wander/WanderModel.java
+++ src/main/java/com/lpoo/fallout/model/wander/WanderModel.java
@@ -1,21 +1,23 @@
 package com.lpoo.fallout.model.wander;
 
 import com.lpoo.fallout.model.Enemy;
+import com.lpoo.fallout.model.Position;
 import com.lpoo.fallout.model.VaultBoy;
 import com.lpoo.fallout.model.Wall;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WanderModel {
-    private final VaultBoy vaultBoy;
+    private VaultBoy vaultBoy;
     private List <Wall> walls;
     private List <Enemy> enemies;
 
-    public WanderModel(VaultBoy vaultBoy) {
+    public WanderModel(@NotNull VaultBoy vaultBoy) {
         this.vaultBoy = vaultBoy;
-        this.walls = new LinkedList<>();
+        this.walls = new ArrayList<>();
+        this.enemies = new ArrayList<>();
     }
 
     public void setWalls(@NotNull List<Wall> walls) {
@@ -26,12 +28,32 @@ public class WanderModel {
         this.enemies = enemies;
     }
 
+    public void setVaultBoy(@NotNull VaultBoy vaultBoy) {
+        this.vaultBoy = vaultBoy;
+    }
+
     public VaultBoy getVaultBoy() {
         return vaultBoy;
     }
 
     public List<Wall> getWalls() {
         return walls;
+    }
+
+    public List<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public boolean isEmpty(@NotNull Position position) {
+        for (Enemy enemy: enemies) {
+            if (enemy.getPosition().equals(position))
+                return false;
+        }
+        for (Wall wall: walls) {
+            if (wall.getPosition().equals(position))
+                return false;
+        }
+        return true;
     }
 
 }
