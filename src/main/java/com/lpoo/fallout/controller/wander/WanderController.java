e8c927bc976c8cc2779d4cb84a015f1df073ee2b
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2021-05-08 16:29:27 +0100

    Updated wander controller to match new MovingEngine position

--- src/main/java/com/lpoo/fallout/controller/wander/WanderController.java
+++ src/main/java/com/lpoo/fallout/controller/wander/WanderController.java
@@ -9,6 +9,8 @@ import com.lpoo.fallout.model.wander.element.VaultBoy;
 import com.lpoo.fallout.view.wander.WanderViewer;
 
 import java.io.IOException;
+import java.util.AbstractList;
+import java.util.AbstractQueue;
 
 public class WanderController implements Controller {
     private WanderModel map;
@@ -22,21 +24,24 @@ public class WanderController implements Controller {
         //this.map = new RandomWanderFactory(10, 10, 4).createWanderModel();
         this.map = new FileWanderFactory("arena1", new VaultBoy(new Position(5, 5),
                 new Attributes(1, 1, 1, 1))).createWanderModel();
-        this.enemyController = new EnemyController(map);
+        this.enemyController = new EnemyController(map, new RandomMovingEngine());
         this.viewer = new WanderViewer(game.getGui(), map);
         this.game = game;
         this.vaultBoyController = new VaultBoyController(map);
+    }
+
+    private Enemy checkFight() {
+        AbstractQueue<Enemy> enemies = map.getOrderedEnemies(map.getVaultBoy().getPosition());
 
     }
 
     @Override
     public void run() throws IOException {
         LanternaGUI.ACTION nextAction = this.game.getGui().getAction();
-        // System.out.println(nextAction);
         if (nextAction == LanternaGUI.ACTION.QUIT) {
             this.game.clearControllers();
         } else {
-            //this.enemyController.moveEnemies();
+            this.enemyController.moveEnemies();
             this.vaultBoyController.action(nextAction);
             this.viewer.draw();
         }
