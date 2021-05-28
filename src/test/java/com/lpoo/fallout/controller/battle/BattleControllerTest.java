2f8a4d6c839cc01e7c6053df1237fc4f9365e562
marhcouto <marhcouto1101@gmail.com>
2021-05-28 14:06:21 +0100

    Testing

--- src/test/java/com/lpoo/fallout/controller/battle/BattleControllerTest.java
+++ src/test/java/com/lpoo/fallout/controller/battle/BattleControllerTest.java
@@ -1,6 +1,7 @@
 package com.lpoo.fallout.controller.battle;
 
 import com.lpoo.fallout.controller.Game;
+import com.lpoo.fallout.model.battle.BattleInfo;
 import com.lpoo.fallout.model.battle.BattleModel;
 import com.lpoo.fallout.model.wander.Arena;
 import com.lpoo.fallout.model.wander.element.Enemy;
@@ -29,13 +30,16 @@ public class BattleControllerTest {
         Game game = Mockito.mock(Game.class);
         VaultBoy vaultBoy = Mockito.mock(VaultBoy.class);
         Enemy enemy = Mockito.mock(Enemy.class);
-        BattleModel battleModel = new BattleModel(Mockito.mock(Arena.class), vaultBoy, enemy);
+        BattleModel battleModel = Mockito.mock(BattleModel.class);
+        BattleInfo battleInfo = Mockito.mock(BattleInfo.class);
         BattleController battleController = new BattleController(battleModel);
 
+        Mockito.when(battleInfo.getVaultBoy()).thenReturn(vaultBoy);
+        Mockito.when(battleModel.getBattleInfo()).thenReturn(battleInfo);
         Mockito.when(battleModel.getBattleInfo().checkDeath()).thenReturn(vaultBoy);
         Assertions.assertTrue(battleController.processDeath(game));
         Mockito.verify(game, Mockito.times(1)).clearStates();
 
-
+        Mockito.when(battleModel.getBattleInfo().checkDeath()).thenReturn(enemy);
     }
 }
